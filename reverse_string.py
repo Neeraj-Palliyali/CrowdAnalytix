a81c8e2b4cf713de2439ddd7c5ef430da54e8d7b
Neeraj-Palliyali <palliyali.neeraj@gmail.com>
2022-05-13 01:02:12 +0530

    reverse string

---
+++ reverse_string.py
@@ -0,0 +1,14 @@
+def reverse_string(input_string:str):
+    split = input_string.split('-')
+    return '-'.join(split[::-1])
+if __name__ == "__main__":
+    try:
+        T = int(input("Enter the number of test cases:"))
+        for test in range(T):
+            try:
+                input_string = input("Enter the input string:")
+                print(reverse_string(input_string))
+            except Exception :
+                print("The input seems to be wrong for a string")
+    except Exception:
+        print("The input should be an integer")
